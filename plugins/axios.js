55ea774ff50db785310adaaccd06e65a339542f3
sadegh <sadeghte@gmail.com>
2019-07-06 14:35:05 +0430

    ui improvement

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,9 @@
 export default function ({ $axios, store }) {
   $axios.onRequest(config => {
+    if(config.url.indexOf('?') < 0)
+      config.url += "?ts="+Date.now();
+    else
+      config.url += "&ts=" + Date.now();
     config.headers.common['locale'] = store.state.locales.locale;
   })
 }
